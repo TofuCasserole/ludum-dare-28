1f9af1d1d41384065a39cc994ebf302e3a5fdbfe
Ian Swift <iswift2@uic.edu>
2013-12-14 01:30:29 -0600

    character moving why not

--- src/Controller/Move.py
+++ src/Controller/Move.py
@@ -9,17 +9,19 @@ RIGHT = "right"
 UP = "up"
 DOWN = "down"
 
-import Model.Room
+import model
 
 def move_character(character, direction):
     (xpos, ypos) = character.location
-    if direction == LEFT and xpos > 0 and (xpos-1, ypos) not in character.room.obstacles:
+    if direction == LEFT and (xpos-1, ypos) not in character.room.obstacles:
         if (xpos-1, ypos) in character.room.monster_locations():
             character.health -= 20
             return
+        if (xpos == 0 and model.EAST in character.room.doors):
+            
         character.loation = (xpos-1,ypos)
         return
-    if direction == RIGHT and xpos < Model.Room.WIDTH and (xpos+1, ypos) not in character.room.obstacles:
+    if direction == RIGHT and xpos < model.WIDTH and (xpos+1, ypos) not in character.room.obstacles:
         if (xpos+1, ypos) in character.room.monster_locations():
             character.health -= 20
             return
@@ -31,7 +33,7 @@ def move_character(character, direction):
             return
         character.loation = (xpos,ypos - 1)
         return
-    if direction == DOWN and ypos < Model.Room.LENGTH and (xpos, ypos - 1) not in character.room.obstacles:
+    if direction == DOWN and ypos < model.LENGTH and (xpos, ypos - 1) not in character.room.obstacles:
         if (xpos, ypos+1) in character.room.monster_locations():
             character.health -= 20
             return
--- src/Model/Character.py
+++ src/Model/Character.py
@@ -4,8 +4,3 @@ Created on Dec 13, 2013
 @author: thedoctor
 '''
 
-class Character:
-    def __init__(self, location, start_room):
-        self.location = location
-        self.health = 100
-        self.room = start_room
