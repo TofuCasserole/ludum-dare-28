123922ab2098dc021f0efaf32be233d38d9a8228
Kristoffe Zehr <kristoffe.zehr@gmail.com>
2013-12-14 22:14:27 -0600

    health bar should be rendering properly, but isn't

--- src/gui.py
+++ src/gui.py
@@ -12,7 +12,18 @@ class HealthBar():
     
     def __init__(self):
         self.sheet = SpriteSheet(pygame.transform.scale2x(model.load_png("gui.png")))
-        self.value = 100
+        self.health = 100
     
     def draw(self, surface):
-        surface.blit(self.sheet.image_at((0,0,132,32)), (0,0))
+        surface.blit(self.sheet.image_at((32,0,64,16)), (0,0))
+        surface.blit(self.sheet.image_at((64,0,96,1)), (0,18))
+        for bar_lvl in range(1,41):
+            y_val = bar_lvl + 18
+            if (bar_lvl % 4) == 0 and bar_lvl != 40:
+                surface.blit(self.sheet.image_at((64,2,96,3)), (0, y_val))
+            else:
+                surface.blit(self.sheet.image_at((64,1,96,2)), (0, y_val))
+            if self.health >= (40 - bar_lvl) * 2.5:
+                surface.blit(self.sheet.image_at((64,3,96,4)), (0, y_val))
+            else:
+                surface.blit(self.sheet.image_at((64,4,96,5)), (0, y_val))
